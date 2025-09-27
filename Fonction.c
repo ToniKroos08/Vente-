0665ee37b70fed94775ab50ba94cc851d7dc387b
ToniKroos08 <pj19569@gmail.com>
2025-09-26 21:07:59 -0400

    Fonction.c

---
+++ Fonction.c
@@ -0,0 +1,14 @@
+include <stdio.h>
+
+int main() {
+    FILE *f = fopen("Hist_vente.dat", "a"); // ouvrir pour écrire à la fin
+    if (f == NULL) {
+        printf("Erreur d'ouverture.\n");
+        return 1;
+    }
+
+    fprintf(f, "Texte ajouté dans le fichier.\n"); // écriture
+    fclose(f);
+
+    return 0;
+}
