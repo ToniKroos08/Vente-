764463c18c73fc7b7e58e6330bb0ee3933cf3252
ToniKroos08 <pj19569@gmail.com>
2025-10-04 00:03:17 -0400

    vente.h

--- vente.h
+++ vente.h
@@ -2,7 +2,7 @@
 /* FCHIER:                          vente.h                               */
 /*AUTEUR:                           JEAN David Kehl                       */    
 /*DATE DE CREATIION:                26/09/2025                            */  
-/*DATE DE MODIFICATION:             27/09/2025                            */ 
+/*DATE DE MODIFICATION:             02/10/2025                            */ 
 /*DESCRIPTION:                      fichier contenant les nouveaux types de
                                     données, les prototypes, les structures 
                                     de données et les constantes          */    
@@ -17,3 +17,8 @@ typedef struct {
     float prix_unit;
     float prix_total;
 } Vente;
+
+// --- Prototypes des fonctions ---
+int mettre_a_jour_stock(int id_prod, int qte_vendue);
+int ajouter_vente(int id_prod, int qte_vendue, float prix_unit);
+void generer_date_courante(char* date_str);
